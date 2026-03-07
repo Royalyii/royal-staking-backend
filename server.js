92eccd551a22400fde4eb9ebd7e48b89f87592f2
Royalyii <israeljabali2000@gmail.com>
2026-03-07 23:04:01 +0300

    Add body-parser middleware to server.js
    Added body-parser middleware for parsing request bodies.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const User = require("./models/User");
+const bodyParser = require("body-parser");
 
 connectDB();
 
@@ -74,4 +75,5 @@ setInterval(async () => {
 
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
